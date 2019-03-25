ad88df5b3ed5d82a655964fd885c1920db8f7fd8
Ngok Chao Ho <ngokchaoho@Ngoks-MacBook-Pro.local>
2019-03-26 04:58:07 +0800

    third

--- calibration.R
+++ calibration.R
@@ -2,16 +2,16 @@ option = read.csv("Options_2.csv",header = TRUE)
 gamma = read.csv("gamma.csv",header = TRUE)
 lambda = read.csv("lambda.csv",header = TRUE)
 option = option[1:7]
-p1_sample = option[option$te-option$t==15,]
+p1_sample = option[option$T-option$te==30,]
 p1_sample = p1_sample[nrow(p1_sample),]
 sigma1_bar = p1_sample$Volatility
-gamma_index = p1_sample$T.te-35+1
-sigma_p1 = sigma1_bar/sqrt(gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2])
+gamma_index = 1
+sigma_t1 = sigma1_bar/sqrt(gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2])
 
-p2_sample = option[option$te-option$t==31,]
+p2_sample = option[option$T-option$te==60,]
 p2_sample = p2_sample[nrow(p2_sample),]
 sigma2_bar = p2_sample$Volatility
-gamma_index = p2_sample$T.te-35+1
-sigma_p2 = sqrt((sigma2_bar^2*31-sigma_p1^2*(gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2])*15)/
-                  (gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2])*(31-15)
+gamma_index = 2
+sigma_t2 = sqrt((sigma2_bar^2*60-sigma1_bar^2*30)/
+                  (gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2])*(60-30)
 )

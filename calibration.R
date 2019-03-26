237ec52a9a7eb35cac3549023ab26bf352c80765
Ngok Chao Ho <ngokchaoho@Ngoks-MacBook-Pro.local>
2019-03-27 00:54:50 +0800

    cail

--- calibration.R
+++ calibration.R
@@ -1,17 +1,23 @@
-option = read.csv("Options_2.csv",header = TRUE)
-gamma = read.csv("gamma.csv",header = TRUE)
-lambda = read.csv("lambda.csv",header = TRUE)
-option = option[1:7]
-p1_sample = option[option$T-option$te==30,]
-p1_sample = p1_sample[nrow(p1_sample),]
-sigma1_bar = p1_sample$Volatility
+gamma = read.csv("gamma_wti.csv",header = TRUE)
+lambda = read.csv("lambda_wti.csv",header = TRUE)
+# t = 17/04/2018
+#also used for kirks'
+sigma1_bar = 0.2517
 gamma_index = 1
+#delta t = 30
 sigma_t1 = sigma1_bar/sqrt(gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2])
-
-p2_sample = option[option$T-option$te==60,]
-p2_sample = p2_sample[nrow(p2_sample),]
-sigma2_bar = p2_sample$Volatility
-gamma_index = 2
-sigma_t2 = sqrt((sigma2_bar^2*60-sigma1_bar^2*30)/
-                  (gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2])*(60-30)
+#> sigma_t1
+#[1] 44.41368
+sigma2_bar = 0.2503
+gamma_index = 1
+#delta t = 59
+sigma_t2 = sqrt((sigma2_bar^2*59-sigma1_bar^2*30)/
+                  (gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2])*(59-30)
 )
+
+#implied volatility for brent in krik's
+gamma = read.csv("gamma_brent.csv",header = TRUE)
+lambda = read.csv("lambda_brent.csv",header = TRUE)
+vol= sqrt(sigma_t1^2*(gamma[gamma_index,2]^2*lambda[1,2]+gamma[gamma_index,3]^2*lambda[2,2]))
+#answer vol used for kirks'
+#[1] 0.2619801
--- new_data_matrix.R
+++ new_data_matrix.R
@@ -2,7 +2,8 @@ library(rlist)
 library(reshape2)
 library(openxlsx)
 data = NULL
-brent = read.csv("combined.csv",header=TRUE)
+#brent actually means WTI.
+brent = read.csv("WTI Futures_2.csv",header=TRUE)
 brent$period = brent$Expiry - brent$t
 unique_period = unique(brent$period)
 #exclude 0
@@ -15,6 +16,7 @@ for (i in 1:length(unique_period)){
   groupped[[i]]=cbind(brent[brent$period==unique_period[i],],ln_diff)
   len = nrow(groupped[[i]]) 
   for (j in 1:len){
+    #delta t is set to be 1 day
     next_step = brent[brent$t==groupped[[i]][j,]$t+1 & brent$Expiry==groupped[[i]][j,]$Expiry,][1,]
     if (is.na(sum(next_step)))
       groupped[[i]][j,]$ln_diff = NA
@@ -31,15 +33,61 @@ delta = NULL
 
 for (i in 1:length(unique_period)) 
   amount[i] = nrow(filtered_groupped[[i]])
-tau = 35:45
+tau = seq(5,8,by = 1)
 #assumed delta t = 1
 for (i in 1:length(tau)){
-  delta=cbind(delta,filtered_groupped[[tau[i]]]$ln_diff[1:11]/1)
+  delta=cbind(delta,filtered_groupped[[tau[i]]]$ln_diff[1:9]/1)
 }
 c = cov(delta)
 result=eigen(c)
 gamma = result$vectors
 lambda = result$values
 cumsum(result$values/sum(result$values))
-write.csv(file="gamma.csv",gamma)
-write.csv(file="lambda.csv",lambda)
+write.csv(file="gamma_wti.csv",gamma)
+write.csv(file="lambda_wti.csv",lambda)
+
+#repeat for brent
+data = NULL
+#brent actually means WTI.
+brent = read.csv("brent Futures_2.csv",header=TRUE)
+brent$period = brent$Expiry - brent$t
+unique_period = unique(brent$period)
+#exclude 0
+unique_period = unique_period[unique_period!=0]
+unique_period = sort(unique_period)
+groupped = vector("list",length(unique_period))
+for (i in 1:length(unique_period)){
+  groupped[[i]]=brent[brent$period==unique_period[i],]
+  ln_diff = seq(1,nrow(groupped[[i]]))
+  groupped[[i]]=cbind(brent[brent$period==unique_period[i],],ln_diff)
+  len = nrow(groupped[[i]]) 
+  for (j in 1:len){
+    #delta t is set to be 1 days
+    next_step = brent[brent$t==groupped[[i]][j,]$t+1 & brent$Expiry==groupped[[i]][j,]$Expiry,][1,]
+    if (is.na(sum(next_step)))
+      groupped[[i]][j,]$ln_diff = NA
+    else
+      groupped[[i]][j,]$ln_diff = log(next_step$Value) - log(groupped[[i]][j,]$Value)
+  }
+}
+
+filtered_groupped = vector("list",length(unique_period))
+for (i in 1:length(unique_period)) {
+  filtered_groupped[[i]] = groupped[[i]][!is.na(groupped[[i]]$ln_diff),]
+}
+delta = NULL
+
+for (i in 1:length(unique_period)) 
+  amount[i] = nrow(filtered_groupped[[i]])
+tau = seq(7,10,by = 1)
+#assumed delta t = 1
+for (i in 1:length(tau)){
+  delta=cbind(delta,filtered_groupped[[tau[i]]]$ln_diff[1:12]/1)
+}
+c = cov(delta)
+result=eigen(c)
+gamma = result$vectors
+lambda = result$values
+cumsum(result$values/sum(result$values))
+write.csv(file="gamma_brent.csv",gamma)
+write.csv(file="lambda_brent.csv",lambda)
